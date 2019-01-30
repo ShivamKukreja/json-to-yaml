31ab5d6a9556031f77c8f5710b039a11386ed7ff
Shivam Kukreja <skukreja@sapient.com>
2019-01-30 19:42:22 +0530

    with promises

--- api/json_to_yaml/index.js
+++ api/json_to_yaml/index.js
@@ -1,51 +1,66 @@
-const fs = require('fs'),
-    path = require('path'),
-    yaml = require('write-yaml'),
-    paths = {
-        READ_JSON: path.resolve('./data/data.json'),
-        WRITE_JSON: path.resolve('./data/results.json'),
-        WRITE_YAML: path.resolve('./data/data.yml')
-    };
-const addKeyValue = (dataArray) => {
-    let length = dataArray.length,
-        index = 0;
-    for (; index < length; index++) {
-        dataArray[index]["size"] = JSON.stringify(dataArray[index]).length;
-    }
-    return dataArray;
-}
-const readFile = (path, callback) => {
+const fs = require("fs"),
+  path = require("path"),
+  yaml = require("write-yaml"),
+  paths = {
+    READ_JSON: path.resolve("./data/data.json"),
+    WRITE_JSON: path.resolve("./data/results.json"),
+    WRITE_YAML: path.resolve("./data/data.yml")
+  };
+const addKeyValue = dataArray => {
+  let length = dataArray.length,
+    index = 0;
+  for (; index < length; index++) {
+    dataArray[index]["size"] = JSON.stringify(dataArray[index]).length;
+  }
+  return dataArray;
+};
+const readFile = path =>
+  new Promise((resolve, reject) => {
     let dataArray = [];
     fs.readFile(path, (err, data) => {
-        if (err) {
-            //callback(err);
-            throw err;
-        }
-        dataArray = addKeyValue(JSON.parse(data));
-        writeFile(dataArray);
-    })
-}
-const writeFile = (data) => {
-    fs.writeFile(paths.WRITE_JSON, JSON.stringify(data, null, 2), (err) => {
-        if (err) throw err;
-        console.log('results are being written in json format');
-        
-    })
-    toYaml(data);
-}
-const toYaml = (data) => {
-    yaml(paths.WRITE_YAML, data, (err) => {
-        if (err) throw err;
-        console.log('results are being written in yaml format');
+      if (err) {
+        //callback(err);
+        // throw err;
+        reject(err);
+      }
+      dataArray = addKeyValue(JSON.parse(data));
+      //writeFile(dataArray);
+      resolve(dataArray);
     });
-}
-const jsonToYaml = () => {
-    readFile(paths.READ_JSON);
-}
+  });
+const writeFile = data =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(paths.WRITE_JSON, JSON.stringify(data, null, 2), err => {
+      if (err) {
+        // throw err;
+        reject(err);
+      }
+      console.log("results are being written in json format");
+      resolve(data);
+    });
+    //toYaml(data);
+  });
+const toYaml = data =>
+  new Promise((resolve, reject) => {
+    yaml(paths.WRITE_YAML, data, err => {
+      if (err) {
+        // throw err;
+        reject(err);
+      }
+      console.log("results are being written in yaml format");
+      resolve(true);
+    });
+  });
+const jsonToYaml = () =>
+  readFile(paths.READ_JSON)
+  .then(data => writeFile(data).then(data => toYaml(data)));
 
+// module.exports = {
+//     jsonToYaml : jsonToYaml,
+//     readFile : readFile,
+//     addKeyValue : addKeyValue
+// }
 module.exports = {
-    jsonToYaml : jsonToYaml,
-    readFile : readFile,
-    addKeyValue : addKeyValue
-}
-/* module.exports = jsonToYaml; */
+  jsonToYaml,
+  addKeyValue
+};
--- test/api-test.js
+++ test/api-test.js
@@ -1,31 +1,29 @@
-const expect = require('chai').expect,
-    api = require('../api');
-describe('api', () => {
-    it('should add key `size` and its value as `JSON.stringify(object).length` to all objects in an array', function () {
-        let testObj = [{
-            name: 'Shivam',
-            age: 28
-        }, {
-            name: 'Bharat',
-            age: 27
-        }];
-        let results = api.addKeyValue(testObj),
-            i = 0,
-            l = results.length;
+const expect = require("chai").expect,
+  api = require("../api");
+describe("api", () => {
+  it("should add key `size` and its value as `JSON.stringify(object).length` to all objects in an array", function() {
+    let testObj = [
+      {
+        name: "Shivam",
+        age: 28
+      },
+      {
+        name: "Bharat",
+        age: 27
+      }
+    ];
+    let results = api.addKeyValue(testObj),
+      i = 0,
+      l = results.length;
 
-        for (; i < l; i++) {
-            expect(results[i]).to.have.own.property('size');
-        }
-        // expect(results).to.be.an('array');
-    })
-    it('should read json', function (done) {
-        api.readFile('./data/test.json', done);
+    for (; i < l; i++) {
+      expect(results[i]).to.have.own.property("size");
+    }
+    // expect(results).to.be.an('array');
+  });
+  it("should write json to yaml", () => {
+    api.jsonToYaml("./data/data.json").then((result) => {
+        expect(result).to.be.true;
     });
-    /* 
-        it('should write json', () => {
-
-        })
-        it('should write yaml', () => {
-
-        }); */
-})
+  });
+});
